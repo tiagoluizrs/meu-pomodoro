a3bd5862c52f9d922a120c1b870644110702391d
Tiago Silva <tiagoluizrs@gmail.com>
2022-10-06 09:20:13 -0300

    Correção state interval

--- pages/Home.js
+++ pages/Home.js
@@ -7,36 +7,49 @@ import {
 const Home = () =>{
     const [minute, setMinute] = useState(25);
     const [seconds, setSeconds] = useState(0);
-    const [startPomodoro, setStartPomodoro] = useState(false)
+    const [startPomodoro, setStartPomodoro] = useState(false);
+    const [intervalMinute, setIntervalMinute] = useState(false);
+    const [intervalSeconds, setIntervalSeconds] = useState(false);
 
     const stopPomodoro = () => {
-        // TODO: Alterar o interval para parar dentro desse método
         setMinute(25);
         setSeconds(0);
+        setStartPomodoro(false);
+
+        // Consegui mexer e alterar. Foi necessário apenas salvar os intervalos como states.
+        clearInterval(intervalMinute);
+        clearInterval(intervalSeconds);
     }
     const initPomodoro = () => {
-        setMinute(24);
-        setSeconds(59);
-        setStartPomodoro(true);
+      setMinute(24);
+      setSeconds(59);
+      setStartPomodoro(true);
 
-        let countMinute = 24;
-        let intervalMinute = setInterval(() => {
-            setMinute(--countMinute);
-            if(countMinute === 0){
-                clearInterval(intervalMinute);
-                clearInterval(intervalSeconds);
-            }
+      let countMinute = 24;
+
+      // O intervalo agora é um state, para que possa ser modificado fora dessa função e reflita no react
+      setIntervalMinute(
+        setInterval(() => {
+          setMinute(--countMinute);
+          if (countMinute === 0) {
+            clearInterval(intervalMinute);
+            clearInterval(intervalSeconds);
+          }
         }, 1000 * 60)
-        
-        let countSecond = 60
-        let intervalSeconds = setInterval(() => {
-            if (countSecond === 0) {
-              setSeconds(59);
-              countSecond = 59;
-            } else {
-              setSeconds(--countSecond);
-            }
+      );
+
+      // O intervalo agora é um state, para que possa ser modificado fora dessa função e reflita no react
+      let countSecond = 60;
+      setIntervalSeconds(
+        setInterval(() => {
+          if (countSecond === 0) {
+            setSeconds(59);
+            countSecond = 59;
+          } else {
+            setSeconds(--countSecond);
+          }
         }, 1000)
+      );
     }
 
     useEffect(() => {
@@ -62,11 +75,11 @@ const Home = () =>{
         >
           {`0${minute}`.slice(-2)}:{`0${seconds}`.slice(-2)}
         </Text>
-        {/* {startPomodoro ? (
+        {startPomodoro ? (
           <Button title="Parar Pomodoro" onPress={stopPomodoro} />
-        ) : ( */}
+        ) : (
           <Button title="Iniciar Pomodoro" onPress={initPomodoro} />
-        {/* )} */}
+        )}
       </View>
     );
 }
